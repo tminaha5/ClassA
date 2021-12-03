67d3b6d24f3d2629716f769c2f6f5c6890a3e418
15718 <15718@LAPTOP-SS0766RE>
2021-12-02 23:54:07 -0500

    hello

--- src/clas/ClassDemo.java
+++ src/clas/ClassDemo.java
@@ -4,7 +4,7 @@ public class ClassDemo {
 	
 	public static void main(String [] args) {
 		
-		System.out.println("Branching and Pulling Request");
+		System.out.println("Branching and Pulling Request...");
 	}
 
 }
